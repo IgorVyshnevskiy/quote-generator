17177257d384bb34626d3140340795058984a515
Igor Vyshnevskyi <igor.vyshnevskiy097@gmail.com>
2023-08-10 16:57:13 +0300

    added some code

---
+++ script.js
@@ -0,0 +1,13 @@
+let apiQuotes = []
+
+async function getQuotes() {
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+  try{
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json()
+    console.log(apiQuotes[12]);
+  }
+  catch(error) {}
+}
+
+getQuotes()
